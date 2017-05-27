ecc41e90692a2d009b8b9ce9dc40d5ac11f8fa2a
Vadim-pmm <v.expmm@gmail.com>
2017-05-27 17:44:10 +0300

    bootstrap and root path set up

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -5,6 +5,8 @@ require('../../scss/style.scss');
 class App extends Component {
 	render() {
 	    return (<div>
+	    	This is our app
+	    	{this.props.children}
 	    </div>
 	}
 };
---
+++ dev/js/components/ArticlesHome.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class ArticlesHome extends Component{
+	render() {
+		return(
+			<div className='container'>
+				Articles Home Page from React !
+			</div>
+			);
+	}
+}
