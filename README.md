efa4ea35b52b3625378f642af7f4da7036d9eb58
Vadim-pmm <v.expmm@gmail.com>
2017-05-27 17:07:43 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React frontend for my Rails API
+
+
---
+++ dev/js/components/App.js
@@ -0,0 +1,12 @@
+import React {Component} from 'react';
+
+require('../../scss/style.scss');
+
+class App extends Component {
+	render() {
+	    return (<div>
+	    </div>
+	}
+};
+
+export default App;
